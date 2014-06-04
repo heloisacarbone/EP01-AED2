81d88263755cd5c1002859081e78c3aaeafadde5
kidtoy <fkenji93@hotmail.com>
2014-06-04 18:04:09 -0300

    Update Aresta.java

--- fefelo.dijkstraGrafos/src/fefelo/dijkstraGrafos/grafo/Aresta.java
+++ fefelo.dijkstraGrafos/src/fefelo/dijkstraGrafos/grafo/Aresta.java
@@ -4,14 +4,14 @@ import fefelo.dijkstraGrafos.grafo.Vertice;
 public class Aresta {
 	private Vertice v1;
 	private Vertice v2;
-	private int peso;
+	private double peso;
 	
 	public Aresta(Vertice v1, Vertice v2) {
 		this.v1 = v1;
 		this.v2 = v2;
 		this.peso = calcpeso(v1, v2);
 	}
-	public int calcpeso(Vertice x, Vertice y){
+	public double calcpeso(Vertice x, Vertice y){
 		int n = (x.getX() - y.getX())^2;
 		int m = (x.getY() - y.getY())^2;
 		return math.sqrt(n+m);
@@ -19,10 +19,6 @@ public class Aresta {
 
 	public int getPeso() {
 		return peso;
-	}
-
-	public void setPeso(int peso) {
-		this.peso = peso;
 	}
 
 	public Vertice getV2() {
