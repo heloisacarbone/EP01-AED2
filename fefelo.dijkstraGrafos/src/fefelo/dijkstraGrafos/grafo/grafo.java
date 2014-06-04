7cda25cd9bc14fc9ae5df57b95eee2803fecb061
kidtoy <fkenji93@hotmail.com>
2014-06-04 20:47:04 -0300

    Update grafo.java

--- fefelo.dijkstraGrafos/src/fefelo/dijkstraGrafos/grafo/grafo.java
+++ fefelo.dijkstraGrafos/src/fefelo/dijkstraGrafos/grafo/grafo.java
@@ -7,16 +7,16 @@ public class grafo {
 	private List<Vertice> adjList = new arrayList<Vertice>();
 
 
-	public startGrafo(List<Vertice> list) {
+	public startGrafo(List<Vertice> list) {		// Inicializa o Grafo com uma lista de vertices
 		this.adjList.addAll(list);
 	}
-	public addvertice(Vertice x){
+	public addvertice(Vertice x){			// Adiciona um Vertice ao Grafo
 		this.adjList.add(x);
 	}
-	public List<Vertice> getgrafo(){
+	public List<Vertice> getgrafo(){		// Retorna a lista com os vertices
 		return this.adjlist;
 	}
-	public Vertice retornavertice(String x){
+	public Vertice retornavertice(String x){		// Retorna um vertice em especifico
 		
 		for(int i = 0; i < getgrafo().size(); i++){
 			if(x.equals(this.getgrafo().get(i).getZ();
