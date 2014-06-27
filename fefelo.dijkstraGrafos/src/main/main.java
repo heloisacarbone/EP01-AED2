f29404cfe677095badb4a4a953e771e719370742
Heloisa Carbone <heloisacarbone@gmail.com>
2014-06-27 17:30:27 -0300

    REfatorando o main em mil classes \o/

--- fefelo.dijkstraGrafos/src/main/main.java
+++ fefelo.dijkstraGrafos/src/main/main.java
@@ -15,8 +15,8 @@ import java.util.Iterator;
 import java.util.HashMap;
 import java.util.Map;
 
-import grafo.Grafo;
-import grafo.Vertice;
+import grafo.*;
+import controllers.*;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -27,237 +27,34 @@ import dijkstra.dijkstraAlgorithm;
 
 public class main {
 
-    static String linha = null;
-    static FileReader arq;
     static String file = "src/entradas/input.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, IOException {
     	
-    	if (args.length == 0) {
-        
+    	if (args.length != 0) {
+    		file = args[0]; 
         }
     	
-        try {
-
-            try {
-            	System.out.println(args[0]);
-                file = args[0];
-            } catch (Exception ex) {
-                System.out.println("Utilizando o arquivo padrão de testes, que se encontra no src/entradas/input, "
-                        + "caso deseja inserir um arquivo diferente passe como argumento na hora de executar "
-                        + "o programa");
-                file = "src/entradas/input.txt";
-            }
-
-            arq = new FileReader(file);
-            System.out.println("arquivo");
-            BufferedReader leitor = new BufferedReader(arq);
-            linha = leitor.readLine(); //Le a  primeira linha 
-
-            //leitura primeira linha 
-            int qtdVert;
-            dijkstraAlgorithm dijk = new dijkstraAlgorithm();
-            Grafo grafo = new Grafo();
-            StringTokenizer valores = new StringTokenizer(linha);
-            String ln1 = valores.nextToken();  
-            qtdVert = Integer.parseInt(ln1);
-                      
-
-            //fim da leitura da 1 linha
-            int[] a = new int[3];
-            int i = 0;
-
-            List<Vertice> listaVertices = new ArrayList<Vertice>();
-            linha = leitor.readLine(); //pula a segunda linha (em branco)            
-            while ((linha = leitor.readLine()) != null) {
-                if (linha.trim().length() == 0) {
-                    break;
-                } //para de ler quando encontra uma linha em branco
-                StringTokenizer vCord = new StringTokenizer(linha);
-                while (vCord.hasMoreTokens()) {
-
-                    String aux = vCord.nextToken();
-                
-                    int cord = Integer.parseInt(aux);
-
-                    a[i] = cord;
-
-                    i++;
-                }
-                String v = String.valueOf(a[0]);
-                Vertice vertice = new Vertice(v, a[1], a[2]);
-                listaVertices.add(vertice);
-               
-                i = 0;
-                // ->>>> Inicializa vertice: Vertice(a[0], a[1], a[2]);
-
-
-                //grafo iniciado
-            }
-            grafo.startGrafo(listaVertices);
-
-            
-
-            List<Vertice> auxListVert = new ArrayList<Vertice>();
-            auxListVert.addAll(listaVertices); //copia a lista de vertices para uma auxiliar
-            int[] c = new int[2];
-            int n = 0;
-            while ((linha = leitor.readLine()) != null) {
-                if (linha.trim().length() == 0) {
-                    break;
-                } //para de ler quando encontra uma linha em branco
-                StringTokenizer adj = new StringTokenizer(linha);
-                while (adj.hasMoreTokens()) {
-                    String aux = adj.nextToken();
-                    int cam = Integer.parseInt(aux);
-                    c[n] = cam;
-                    n++;
-                }
-
-                Iterator it = listaVertices.listIterator();
-                String c0 = String.valueOf(c[0]);
-                String c1 = String.valueOf(c[1]);
-
-                while (it.hasNext()) {
-                    Vertice vertice = (Vertice) it.next();
-                    String desc = vertice.getZ();
-                    Iterator it2 = listaVertices.listIterator();
-                    Vertice compara = (Vertice) it2.next();
-                    String descCompara = compara.getZ();
-                    if (desc.equals(c0)) {   //enquanto o vertice da lista de vertices for diferente do que esta no input
-                        while (!descCompara.equals(c1)) {
-                            compara = (Vertice) it2.next();
-                            descCompara = compara.getZ();
-                            //sai do loop quando encontrar o c1, adjacente a c0
-
-                        }
-                        vertice.setadj(compara);
-                    }
-
-                }
-                Iterator it1 = listaVertices.listIterator();
-                while (it1.hasNext()) {
-                    Vertice vertice = (Vertice) it1.next();
-                    String desc = vertice.getZ();
-                    Iterator it2 = listaVertices.listIterator();
-                    Vertice compara = (Vertice) it2.next();
-                    String descCompara = compara.getZ();
-                    if (desc.equals(c1)) {   //enquanto o vertice da lista de vertices for diferente do que esta no input
-                        while (!descCompara.equals(c0)) {
-                            compara = (Vertice) it2.next();
-                            descCompara = compara.getZ();
-                            //sai do loop quando encontrar o c0, adjacente a c1
-
-                        }
-                        vertice.setadj(compara);
-                    }
-
-                }
-
-             
-
-                n = 0;
-            }
-
-            //le o caminhoa ser feito
-            int[] b = new int[2];
-            int h = 0;
-            while ((linha = leitor.readLine()) != null) {
-                StringTokenizer adj = new StringTokenizer(linha);
-                while (adj.hasMoreTokens()) {
-                    String aux = adj.nextToken();
-                    int cam = Integer.parseInt(aux);
-                    b[h] = cam;
-                    h++;
-                }
-               
-
-                h = 0;
-            }
-
-            String b0 = String.valueOf(b[0]);
-            String b1 = String.valueOf(b[1]);
-            for (Vertice vertex : listaVertices) {
-                String a1 = vertex.getZ();
-                if (a1.equals(b0)) {
-                    dijk.inicio(vertex);
-                }
-
-                if (a1.equals(b1)) {
-                    dijk.fim(vertex);
-                }
-            }
-
-
-           
-
-
-            List<String> listKey = new ArrayList<String>();
-            List<String> listVal = new ArrayList<String>();
-            dijk.dijkstraAlgorithm(grafo);
-            Map<String, String> caminho2 = new HashMap<String, String>();
-            caminho2 = dijk.mapa(grafo);
-            double maxdist = dijk.maxdistancegrafo(grafo);
-
-            File fileOut = new File("src/saidas/output.txt");
-            System.out.println("Saida");
-            Iterator itKey = caminho2.keySet().iterator();
-            Iterator itVal = caminho2.values().iterator();
-            while (itKey.hasNext()) {
-                Object k = itKey.next();
-                String key = String.valueOf(k);
-                listKey.add(key);
-            }
-            while (itVal.hasNext()) {
-                Object v = itVal.next();
-                String val = String.valueOf(v);
-                listVal.add(val);
-            }
-            if (fileOut.exists()) {
-                fileOut.delete();
-            } else {
-                fileOut.createNewFile();
-            }
-            System.out.println("Pre");
-            FileWriter out = new FileWriter(fileOut, true);
-            System.out.println("FILEE");
-            PrintWriter printWriter = new PrintWriter(out);
-            printWriter.println("=================================");
-            ListIterator ltKey = listKey.listIterator(listKey.size());
-            ListIterator ltVal = listVal.listIterator(listVal.size());
-            String Imprime = "";
-            while (ltKey.hasPrevious()) {
-                String ltk = String.valueOf(ltKey.previous());
-                Imprime += (ltk + " ");
-                while (ltVal.hasPrevious()) {
-                    String ltv = String.valueOf(ltVal.previous());
-                    Imprime += (ltv);
-                    ltVal.remove();
-                    break;
-                }
-                printWriter.println(Imprime);
-                Imprime = "";
-                ltKey.remove();
-            }
-            printWriter.println("");
-
-
-            printWriter.println((int) maxdist + 1);
-            printWriter.print("=================================");
-            out.close(); // cria o arquivo 
-            System.out.println("chegou aqui");
-            leitor.close();
-            arq.close();
-
-        } catch (FileNotFoundException ex) {
-            System.out.println("Arquivo não encontrado");
-        } catch (IOException ex) {
-            System.out.println(ex);
-        }
-
-
+    	if (file.indexOf(".txt") != -1) {
+    		try {
+    			Controller.initializeGraphTXT(file);
+    		} catch (Exception ex) {
+    			ex.printStackTrace();
+    		}
+    		
+    	} else if (file.indexOf(".xml") != -1) {
+    		try {
+    			Controller.initializeGraphXML(file);
+    		} catch (Exception ex) {
+    			ex.printStackTrace();
+    		}
+    	} else {
+    		System.out.println("O arquivo inserido não é valido, é preciso que seja xml ou txt.");
+    		System.exit(0);
+    	}
 
     }
+       
 }
     	
     	
